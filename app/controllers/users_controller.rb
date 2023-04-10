b7011b03c0b632f1afa032a0a9154498bf0c0ac1
mptnnk <amzi1221@gmail.com>
2023-04-10 07:07:55 +0000

    test done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :ensure_correct_user, only: [:update]
+  # before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
     @user = User.find(params[:id])
@@ -15,9 +15,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to user_path(current_user.id)
-    end
+    # unless @user == current_user
+    #   redirect_to user_path(current_user.id)
+    # end
   end
 
   def update
